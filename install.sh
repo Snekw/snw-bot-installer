8edaa5114decd8d7bb5e2eec8271254f7a9f1278
Snekw <dev.snekw@gmail.com>
2016-05-23 22:33:06 +0300

    Script fixing

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir /home/snwbot
 mkdir /home/snwbot/scripts
 cp -r ./scripts /home/snwbot
 cd /home/snwbot/scripts
-chmod +x ./installDeb.sh
+chmod +x ./installDep.sh
 chmod +x ./run.sh
 chmod +x ./update.sh
 
