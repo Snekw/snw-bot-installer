6540b5a17732d1d56b8d57b497959317dd0711c8
Snekw <dev.snekw@gmail.com>
2016-05-23 22:32:03 +0300

    Script fixing

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 mkdir /home/snwbot
 mkdir /home/snwbot/scripts
 cp -r ./scripts /home/snwbot
-chmod +x /home/snwbot/scripts/installDeb.sh
-chmod +x /home/snwbot/scripts/run.sh
-chmod +x /home/snwbot/scripts/update.sh
+cd /home/snwbot/scripts
+chmod +x ./installDeb.sh
+chmod +x ./run.sh
+chmod +x ./update.sh
 
 useradd -m -c "Snw-bot" snwbot -s /user/sbin/nologin
 
-sudo /home/snwbot/scripts/installDep.sh
+sudo ./installDep.sh
